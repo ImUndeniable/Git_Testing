f67cc1552e1ef1620b8249c7c4f39ea4858fa340
ImUndeniable <tarun282000@gmail.com>
2025-10-17 22:19:13 +0530

    edit

--- main.go
+++ main.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 )
 
+
 func main(){
-	fmt.Println("Hello World")
-		
+	var x = "Wassap!"
+	Hello(x)
+
+	var a int = 5
+	var b int = 6
+	var result = Add(a,b)
+	fmt.Println(result)
+	
+
 }
+
+
+//calling function
+func Hello(x string){
+	fmt.Println("Yooo",x)
+}
+
+// return function
+func Add(a int ,  b int) int{
+	var result int = a + b
+	return result
+} 
